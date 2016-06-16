82d9e06a2d94e7be94e5e4aee135666f970461f4
Matthew Carter <m@ahungry.com>
2016-06-16 00:12:33 -0400

    Add license to each file

--- config.h
+++ config.h
@@ -1,3 +1,25 @@
+// Copyright (C) 2015,2016  Matthew Carter <m@ahungry.com>
+
+// Author: Matthew Carter <m@ahungry.com>
+// Maintainer: Matthew Carter <m@ahungry.com>
+// URL: https://github.com/ahungry/puny-search
+// Version: 0.0.0
+
+// License:
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #define LOCATE_CMD "/usr/bin/locate -l 30 -A "
 #define OPEN_FILE_CMD "/usr/bin/thunar "
 #define STAT_FILE_CMD "/usr/bin/stat "
--- puny-search.c
+++ puny-search.c
@@ -1,5 +1,33 @@
+// Copyright (C) 2015,2016  Matthew Carter <m@ahungry.com>
+
+// Author: Matthew Carter <m@ahungry.com>
+// Maintainer: Matthew Carter <m@ahungry.com>
+// URL: https://github.com/ahungry/puny-search
+// Version: 0.0.0
+
+// License:
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// Commentary:
+
+// Basic socket implementation taken from (GNU Documentation License v2)
 // http://rosettacode.org/wiki/Echo_server#C
+
+// A guide with more info on network/socket programming in C
 // http://beej.us/guide/bgnet/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
